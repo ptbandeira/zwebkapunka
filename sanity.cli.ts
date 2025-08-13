d0e99f5e2b3e356d7c8a3d84d00301aaa8382c34
Flagster <63302966+ptbandeira@users.noreply.github.com>
2025-08-13 11:35:23 +0000

    sanity id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'your-project-id', // Same as above
+    projectId: 'hqs5j1qe', // Same as above
     dataset: 'production',
   },
 })
